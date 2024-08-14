e0c27ed623b9413211998d1dd496f68832ac675f
Veronika <xafling@mail.ru>
2024-08-14 13:37:35 +0300

    added eslint+prettier

---
+++ eslint.config.cjs
@@ -0,0 +1,43 @@
+module.exports = [
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    ignores: ['dist', '.eslintrc.cjs'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: require('@typescript-eslint/parser'),
+      globals: {
+        window: true,
+        document: true,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      react: require('eslint-plugin-react'),
+      'react-hooks': require('eslint-plugin-react-hooks'),
+      prettier: require('eslint-plugin-prettier'),
+      'react-refresh': require('eslint-plugin-react-refresh'),
+    },
+    rules: {
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'prettier/prettier': [
+        'warn',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  },
+];
