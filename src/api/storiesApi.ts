6f8e1172bbff0e79810a82b1d39f59287c9cad9d
Veronika <xafling@mail.ru>
2024-09-17 16:45:17 +0300

    added storiesApi

---
+++ src/api/storiesApi.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosResponse } from 'axios';
+
+// Устанавливаем URL API
+const API_URL = process.env.VITE_API_BASE_URL as string;
+
+// Эндпоинт для работы с историями
+const storiesEndpoint = `${API_URL}/stories/`;
+
+// Интерфейс для данных истории
+interface IStory {
+  id: number;
+  link: string;
+  link_name?: string;
+  cover?: string | null;
+  title?: string | null;
+  text?: string | null;
+  media_files?: string | null;
+  background?: string | null;
+  hidden?: boolean;
+}
+
+// Типизация для параметров фильтрации
+interface IGetStoriesParams {
+  hidden?: boolean;
+}
+
+// Получение списка историй с возможным фильтром
+export const getStories = async (
+  params?: IGetStoriesParams,
+): Promise<IStory[]> => {
+  try {
+    const response: AxiosResponse<IStory[]> = await axios.get(storiesEndpoint, {
+      params: params,
+    });
+    return response.data;
+  } catch (error: any) {
+    console.error('Error fetching stories:', error);
+    throw new Error('Failed to fetch stories');
+  }
+};
