e7f821927dde5f2d57c613bd84e79d42a32e717d
Veronika <xafling@mail.ru>
2024-10-06 14:52:29 +0300

    fixed error

--- src/pages/Curator/CuratorPage.tsx
+++ src/pages/Curator/CuratorPage.tsx
@@ -2,18 +2,11 @@ import React from 'react';
 import TabBar from '../../components/TabBar/TabBar';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 
-interface ICuratorPageProps {
-  userRole: 'curator' | 'volunteer';
-}
-
-const CuratorPage: React.FC<ICuratorPageProps> = ({ userRole }) => {
-  const variant = userRole === 'curator' ? 'mainScreen' : 'volunteerForm';
-
+const CuratorPage: React.FC = () => {
   return (
     <>
-      <NavigationBar variant={variant} />{' '}
-      {/* <NavigationBar variant="mainScreen" /> */}
-      <TabBar userRole={userRole} />
+      <NavigationBar variant="mainScreen" />
+      <TabBar userRole="curator" />
     </>
   );
 };
