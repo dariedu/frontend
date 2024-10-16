ab45d58d0049f199fab4483eee4118cf5e705a49
Veronika <xafling@mail.ru>
2024-10-16 22:45:04 +0300

    tuned

--- src/core/getQueryParams.ts
+++ src/core/getQueryParams.ts
@@ -1,9 +1,17 @@
-export const getQueryParams = () => {
-  const params = new URLSearchParams(window.location.search);
+export const getTelegramParams = () => {
+  if (window.Telegram?.WebApp?.initDataUnsafe) {
+    const initData = window.Telegram.WebApp.initDataUnsafe;
+
+    return {
+      phone: initData.user?.phone_number || null,
+      tgId: initData.user?.id || null,
+      tgUsername: initData.user?.username || null,
+    };
+  }
 
   return {
-    phone: params.get('phone_number'),
-    tgId: params.get('tg_id'),
-    tgUsername: params.get('tg_nickname'),
+    phone: null,
+    tgId: null,
+    tgUsername: null,
   };
 };
