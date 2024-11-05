28b261f97851ac868dc28956ff79356f232fcab7
MargoG197 <goncharovarita@mail.ru>
2024-11-05 13:02:21 +0300

    fixed bug in CardDelivery, was missing closing div tag

--- src/components/ui/Cards/CardTask/CardDelivery.tsx
+++ src/components/ui/Cards/CardTask/CardDelivery.tsx
@@ -18,21 +18,21 @@ type TCardDeliveryProps = {
 };
 
 const CardDelivery: React.FC<TCardDeliveryProps> = ({ delivery, canBook, switchTab,  getDelivery, stringForModal, takeDeliverySuccess, setTakeDeliverySuccess}) => {
-const [isOpen, setIsOpen] = useState(false);
+ const [isOpen, setIsOpen] = useState(false);
 
-  const deliveryDate = new Date(delivery.date);
-  const hours =
+   const deliveryDate = new Date(delivery.date);
+   const hours =
     deliveryDate.getHours() < 10
       ? '0' + deliveryDate.getHours()
       : deliveryDate.getHours();
-  const minutes =
+    const minutes =
     deliveryDate.getMinutes() < 10
       ? '0' + deliveryDate.getMinutes()
       : deliveryDate.getMinutes();
 
-  return (
-    <>
-      <div className="p-4 bg-light-gray-1 rounded-2xl shadow w-[240px] h-[116px] mb-4 flex flex-col justify-between dark:bg-light-gray-6" onClick={()=>setIsOpen(true)}>
+     return (
+     <>
+     <div className="p-4 bg-light-gray-1 rounded-2xl shadow w-[240px] h-[116px] mb-4 flex flex-col justify-between dark:bg-light-gray-6" onClick={()=>setIsOpen(true)}>
       <div className="flex items-center">
         <div className="flex items-start justify-center">
           <img src='./../src/assets/icons/metro_station.svg' alt="task-icon" className="w-[32px] h-[32px]" />
@@ -44,7 +44,8 @@ const [isOpen, setIsOpen] = useState(false);
           <p className='text-light-gray-black font-gerbera-sub1 dark:text-light-gray-3'>{delivery.location.address}</p>
 
           </div>
-        </div>
+             </div>
+            </div>    
         <div className="flex justify-between items-center w-[133px]">
         <div className="flex items-center justify-center bg-light-gray-white w-[54px] h-[28px] rounded-full dark:bg-light-gray-5 dark:text-light-gray-2">
           {`${hours}:${minutes}`}
@@ -55,12 +56,15 @@ const [isOpen, setIsOpen] = useState(false);
             </span>
           </div>
         </div>
-      </div>
+           </div>
+           
       <Modal isOpen={isOpen} onOpenChange={setIsOpen}>
        <DetailedInfoDelivery delivery={delivery} canBook={canBook} switchTab={switchTab} isOpen={isOpen} onOpenChange={setIsOpen} getDelivery={getDelivery} stringForModal={stringForModal} takeDeliverySuccess={takeDeliverySuccess} setTakeDeliverySuccess={setTakeDeliverySuccess} />
       </Modal>
-    </>
-  );
-};
+          
+         </>
+         )
+
+   };
 
 export default CardDelivery;
