b4eef4a49a6847012f255c383da93839738116e9
MargoG197 <goncharovarita@mail.ru>
2025-02-24 15:49:17 +0300

    added activate delivery

--- src/components/RouteSheets/RouteSheetsCurator.tsx
+++ src/components/RouteSheets/RouteSheetsCurator.tsx
@@ -22,7 +22,9 @@ interface RouteSheetsProps {
   deliveryId: number
   assignedRouteSheets: IRouteSheetAssignments[]
   changeAssignedRouteSheets: () => {}
-  completeDeliveryFunc: (deliveryId: number)=> Promise<void>
+  completeDeliveryFunc: (deliveryId: number) => Promise<void>
+  activateDeliveryFunc: (deliveryId: number) => Promise<void>
+  setCurrentStatus:React.Dispatch<React.SetStateAction<'nearest' | 'active' | 'completed'>>
 }
 
 const RouteSheetsM: React.FC<RouteSheetsProps> = ({
@@ -35,7 +37,9 @@ const RouteSheetsM: React.FC<RouteSheetsProps> = ({
   deliveryId,
   assignedRouteSheets,
   changeAssignedRouteSheets,
-  completeDeliveryFunc
+  completeDeliveryFunc,
+  activateDeliveryFunc,
+  setCurrentStatus
 }) => {
 
 
@@ -52,8 +56,9 @@ const RouteSheetsM: React.FC<RouteSheetsProps> = ({
   const [filtered, setFiltered] = useState<IfilteredRouteSheet[]>([])
   const [filteredSuccess, setFilteredSuccess] = useState(false)
   const [askCuratorCompleteDelivery, setAskCuratorCompleteDelivery] = useState(false)
+  const [askCuratorActivateDelivery, setAskCuratorActivateDelivery] = useState(false)
   const [myPhotoReports, setMyPhotoReports] = useState<TServerResponsePhotoReport[]>(localStorage.getItem(`curator_del_${deliveryId}`) !== null && localStorage.getItem(`curator_del_${deliveryId}`) !== undefined ? JSON.parse(localStorage.getItem(`curator_del_${deliveryId}`) as string) : []);
-
+  // const [deliveryStatus, setDeliveryStatus]= useState<'Активная' | 'Ближайшая' | 'Завершенная' >(status)
 
   ///// используем контекст токена
   const tokenContext = useContext(TokenContext);
@@ -176,17 +181,39 @@ const RouteSheetsM: React.FC<RouteSheetsProps> = ({
           }
         }}>
         {currentStatus == 'completed' ? "Завершена" : "Завершить доставку"} 
+        </button>}
+        {status == 'Ближайшая' &&
+        <button className={currentStatus == 'active'? "btn-B-GreenInactive mt-10 self-center" : 'btn-B-GreenDefault mt-10 self-center'} onClick={() => {
+          if (currentStatus == 'active') { 
+            ()=>{}
+          } else {
+            setAskCuratorActivateDelivery(true)
+          }
+        }}>
+        {currentStatus == 'active' ? "Активирована" : "Активировать доставку"} 
        </button>}
       <ConfirmModal
         isOpen={askCuratorCompleteDelivery}
         onOpenChange={setAskCuratorCompleteDelivery}
-        onConfirm={() => { completeDeliveryFunc(deliveryId); setAskCuratorCompleteDelivery(false)} }
+        onConfirm={() => { completeDeliveryFunc(deliveryId); setAskCuratorCompleteDelivery(false);  setCurrentStatus('completed')} }
         onCancel={() => setAskCuratorCompleteDelivery(false)}
         title="Вы уверены, что хотите завершить доставку?"
         cancelText='Закрыть'
         description=""
         confirmText="Завершить"
         isSingleButton={false}
+      />
+      <ConfirmModal
+        isOpen={askCuratorActivateDelivery}
+        onOpenChange={setAskCuratorActivateDelivery}
+        onConfirm={() => { activateDeliveryFunc(deliveryId); setAskCuratorActivateDelivery(false);  setCurrentStatus('active') } }
+        onCancel={() => setAskCuratorActivateDelivery(false)}
+        title="Вы уверены, что хотите активировать эту доставку?"
+        cancelText='Закрыть'
+        description=""
+        confirmText="Активировать"
+        isSingleButton={false}
+
       />
       
     </div>)
