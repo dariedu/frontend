a0a2491f7e8980a0827517a82ade7ade1bb1e6b3
MargoG197 <goncharovarita@mail.ru>
2025-03-22 11:20:21 +0300

    added text redactor for slideStories title

--- src/components/ui/Cards/CardStories/CardStories.tsx
+++ src/components/ui/Cards/CardStories/CardStories.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as Avatar from '@radix-ui/react-avatar';
+import { useState } from 'react';
+import TextEdit, { findTextPosition } from '../../../TextEdit/TextEdit';
 
 // Интерфейс пропсов для компонента CardStories
 interface CardStoriesProps {
@@ -13,6 +15,37 @@ const CardStories: React.FC<CardStoriesProps> = ({
   title,
   onClick,
 }) => {
+
+  const [positionV, setPositionV] = useState<"top-[12px]" | "top-[55px]" | "bottom-[12px]" >("bottom-[12px]");
+  const [positionH, setPositionH] = useState<"left" | "center" | "right" | "justify">('left');
+  const [finalTitle, setFinalTitle] = useState<string>(title);
+
+
+  // <vt> - наверху
+  // <vm> - посередине
+  
+  ///определяем вертикальное позиционирование для этого компонента
+function findTextVerticalPosition(text: string) {
+  const match = text.match(/<(vt|vm)>/);
+  let position: "top-[12px]" | "top-[55px]" | "bottom-[12px]"  = "bottom-[12px]" ;
+  if (match) {
+     position = match[0] == "<vm>" ? "top-[55px]" : match[0] == "<vt>" ? "top-[12px]" : "bottom-[12px]";
+  }
+  text.replace(/<(vt|vm)>/, "");
+  setFinalTitle(text)
+  setPositionV(position)
+}
+
+useEffect(() => {
+  findTextVerticalPosition(title)
+},[])
+  
+
+  useEffect(() => {
+  findTextPosition(title, setPositionH)
+},[])
+
+
   return (
     <div
       className="relative w-[116px] h-[160px] cursor-pointer overflow-hidden"
@@ -24,8 +57,8 @@ const CardStories: React.FC<CardStoriesProps> = ({
           </Avatar.Fallback>
         </Avatar.Root>
       {/* Текст поверх картинки */}
-      <div className="absolute bottom-[12px] px-[12px] font-gerbera-sub2 text-light-gray-white w-full h-fit whitespace-normal text-left">
-        {title}
+      <div className={`absolute text-${positionH} ${positionV} px-[12px] font-gerbera-sub2 text-light-gray-white w-full h-fit whitespace-normal `}>
+        <TextEdit text={finalTitle} />
       </div>
     </div>
   );
